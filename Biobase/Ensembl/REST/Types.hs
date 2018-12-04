320bfe84f20acc7a585b54cb22b512b3cc8904af
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2018-12-04 16:57:17 +0100

    Ensembl http query ds now uses text

--- Biobase/Ensembl/REST/Types.hs
+++ Biobase/Ensembl/REST/Types.hs
@@ -5,17 +5,18 @@ module Biobase.Ensembl.REST.Types (
 import Data.Aeson
 import GHC.Generics
 import Data.Maybe
+import qualified Data.Text as T
 
 --"[{\"display_id\":\"00300+2.7.2.4+1.1.1.3\",\"primary_id\":\"00300+2.7.2.4+1.1.1.3\",\"version\":\"0\",\"description\":\"\",\"dbname\":\"KEGG_Enzyme\",\"synonyms\":[],\"info_text\":\"\",\"info_type\":\"NONE\",\"db_display_name\":\"KEGG Pathway and Enzyme\"}]""
 
 data EnsemblEntry = EnsemblEntry
-      { display_id :: Maybe String
-      , primary_id :: Maybe String
-      , version :: Maybe String
-      , description :: Maybe String
-      , dbname :: Maybe String
-      , synonyms :: [String]
-      , info_text :: Maybe String
-      , info_type :: Maybe String
-      , db_display_name :: Maybe String
+      { display_id :: Maybe T.Text
+      , primary_id :: Maybe T.Text
+      , version :: Maybe T.Text
+      , description :: Maybe T.Text
+      , dbname :: Maybe T.Text
+      , synonyms :: [T.Text]
+      , info_text :: Maybe T.Text
+      , info_type :: Maybe T.Text
+      , db_display_name :: Maybe T.Text
       } deriving (Show, Generic, ToJSON, FromJSON)
